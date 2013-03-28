33a25edad01ce9bea4ec8c51b085e8c487e82677
Gert Villemos <gvillemos@gmail.com>
2013-03-28 21:18:11 +0100

    Created bat files for windows.

--- systems/estcube/src/main/scripts/startGroundStation.bat
+++ systems/estcube/src/main/scripts/startGroundStation.bat
@@ -1,1 +1,2 @@
-java "-Dhbird.assembly=assemblies/groundstation-es5ec.xml" "-Dhbird.scriptlibrary=scripts" "-Dorekit.data.path=resources" "-Dlog4j.configuration=file:resources/log4j.properties" "-Dhbird.log=groundstation" -jar hbird--0.7.0-SNAPSHOT.jar 
+java "-Dhbird.assembly=assemblies/groundstation-es5ec.xml" "-Dhbird.scriptlibrary=scripts" "-Dorekit.data.path=resources" "-Dlog4j.configuration=file:resources/log4j.properties" "-Dhbird.log=groundstation" -jar hbird-0.7.0-SNAPSHOT.jar 
+pause
