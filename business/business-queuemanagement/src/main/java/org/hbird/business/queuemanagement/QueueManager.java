6696e3f9d4700560e40fd29e7e5e509bd1ee7692
Lauri Kimmel <lauri.kimmel@gmx.com>
2013-04-03 12:21:18 +0300

    Removed references to the business-api module.

--- business/business-queuemanagement/src/main/java/org/hbird/business/queuemanagement/QueueManager.java
+++ business/business-queuemanagement/src/main/java/org/hbird/business/queuemanagement/QueueManager.java
@@ -23,6 +23,7 @@ import java.util.Map;
 
 import javax.jms.InvalidSelectorException;
 import javax.management.MalformedObjectNameException;
+import javax.management.openmbean.OpenDataException;
 
 import org.apache.camel.Body;
 import org.hbird.business.api.IQueueManagement;
@@ -31,8 +32,6 @@ import org.hbird.queuemanagement.ClearQueue;
 import org.hbird.queuemanagement.RemoveQueueElements;
 import org.hbird.queuemanagement.ViewQueue;
 
-import javax.management.openmbean.OpenDataException;
-
 /**
  * The queue manager component support the monitoring and control of a activemq queue.
  * 
@@ -40,70 +39,71 @@ import javax.management.openmbean.OpenDataException;
  * the queue manager to run centrally, with 'commands' being send to it.
  * 
  * @author Gert Villemos
- *
+ * 
  */
 public class QueueManager {
 
-	/** The connection to the server. */
-	protected IQueueManagement api = null;
-	
-	protected String name;
-	
-	public QueueManager(String name) {
-		this.name = name;
-		this.api = new QueueManagerApi(name);
-	}
-	
-	/**
-	 * Method to list all queues in the system.
-	 * 
-	 * @return List of all queues. The list is a pretty printed queue.
-	 * @throws MalformedObjectNameException
-	 * @throws MalformedURLException
-	 * @throws NullPointerException
-	 * @throws IOException
-	 */
-	public List<String> listQueues() throws MalformedObjectNameException, MalformedURLException, NullPointerException, IOException {
-		return api.listQueues();
-	}
+    /** The connection to the server. */
+    protected IQueueManagement api = null;
 
+    protected String name;
 
-	/**
-	 * Method to get all elements of a specific queue
-	 * 
-	 * @param command Command requesting the listing. Holds the name of the queue.
-	 * @return
-	 * @throws InvalidSelectorException
-	 * @throws OpenDataException
-	 * @throws MalformedObjectNameException
-	 * @throws MalformedURLException
-	 * @throws NullPointerException
-	 * @throws IOException
-	 */
-	public Map<String, String> viewQueue(@Body ViewQueue command) throws InvalidSelectorException, OpenDataException, MalformedObjectNameException, MalformedURLException, NullPointerException, IOException {
-		return api.viewQueue(command.getQueueName());
-	}
+    public QueueManager(String name) {
+        this.name = name;
+        this.api = new QueueManagerApi(name);
+    }
 
-	/**
-	 * Method to remove a specific set of entries from a queue.
-	 * 
-	 * @param command The command to remove elements. Contains the JMS ID of the elements to delete and the queue name.
-	 * @return A Map defining the current set of elements.
-	 * @throws Exception
-	 */
-	public Map<String, String> removeQueueElements(@Body RemoveQueueElements command) throws Exception {
-		return api.removeQueueElements(command.getQueueName(), command.getPattern());
-	}
+    /**
+     * Method to list all queues in the system.
+     * 
+     * @return List of all queues. The list is a pretty printed queue.
+     * @throws MalformedObjectNameException
+     * @throws MalformedURLException
+     * @throws NullPointerException
+     * @throws IOException
+     * @throws Exception
+     */
+    public List<String> listQueues() throws MalformedObjectNameException, MalformedURLException, NullPointerException, IOException, Exception {
+        return api.listQueues();
+    }
 
+    /**
+     * Method to get all elements of a specific queue
+     * 
+     * @param command Command requesting the listing. Holds the name of the queue.
+     * @return
+     * @throws InvalidSelectorException
+     * @throws OpenDataException
+     * @throws MalformedObjectNameException
+     * @throws MalformedURLException
+     * @throws NullPointerException
+     * @throws IOException
+     * @throws Exception
+     */
+    public Map<String, String> viewQueue(@Body ViewQueue command) throws InvalidSelectorException, OpenDataException, MalformedObjectNameException,
+            MalformedURLException, NullPointerException, IOException, Exception {
+        return api.viewQueue(command.getQueueName());
+    }
 
-	/**
-	 * Method to delete all entries in a queue.
-	 * 
-	 * @param command The clear command. Contains the name of the queue to be purged.
-	 * @return True
-	 * @throws Exception
-	 */
-	public Boolean clearQueue(@Body ClearQueue command) throws Exception {
-		return api.clearQueue(command.getQueueName());
-	}
+    /**
+     * Method to remove a specific set of entries from a queue.
+     * 
+     * @param command The command to remove elements. Contains the JMS ID of the elements to delete and the queue name.
+     * @return A Map defining the current set of elements.
+     * @throws Exception
+     */
+    public Map<String, String> removeQueueElements(@Body RemoveQueueElements command) throws Exception {
+        return api.removeQueueElements(command.getQueueName(), command.getPattern());
+    }
+
+    /**
+     * Method to delete all entries in a queue.
+     * 
+     * @param command The clear command. Contains the name of the queue to be purged.
+     * @return True
+     * @throws Exception
+     */
+    public Boolean clearQueue(@Body ClearQueue command) throws Exception {
+        return api.clearQueue(command.getQueueName());
+    }
 }
