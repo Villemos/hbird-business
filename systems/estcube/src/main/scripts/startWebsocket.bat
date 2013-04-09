e45996c470d5b2fbcf7b4e6723f751abda387dfc
Lauri Kimmel <lauri.kimmel@gmx.com>
2013-04-09 18:09:24 +0300

    Updated version to 0.7.1-SNAPSHOT.

--- systems/estcube/src/main/scripts/startWebsocket.bat
+++ systems/estcube/src/main/scripts/startWebsocket.bat
@@ -1,2 +1,2 @@
-java "-Dhbird.assembly=assemblies/websockets.xml" "-Dhbird.scriptlibrary=scripts" "-Dorekit.data.path=resources" "-Dlog4j.configuration=file:resources/log4j.properties" "-Dhbird.log=ui" -jar hbird-0.7.0-SNAPSHOT.jar 
+java "-Dhbird.assembly=assemblies/websockets.xml" "-Dhbird.scriptlibrary=scripts" "-Dorekit.data.path=resources" "-Dlog4j.configuration=file:resources/log4j.properties" "-Dhbird.log=ui" -jar hbird-0.7.1-SNAPSHOT.jar 
 pause
