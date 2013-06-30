842bc93d05d3a65f5c51d366712d46af56fd310f
Lauri Kimmel <lauri.kimmel@gmx.com>
2013-07-01 01:40:50 +0300

    Release 0.9.0
    * Updated version from 0.9.0-SNAPSHOT to 0.9.0

--- systems/estcube/src/main/scripts/startGroundStation.bat
+++ systems/estcube/src/main/scripts/startGroundStation.bat
@@ -1,2 +1,2 @@
-java "-Dhbird.assembly=assemblies/groundstation-darmstadt.xml" "-Dhbird.scriptlibrary=scripts" "-Dorekit.data.path=resources" "-Dlog4j.configuration=file:resources/log4j.properties" "-Dhbird.log=groundstation" -jar hbird-0.9.0-SNAPSHOT.jar 
+java "-Dhbird.assembly=assemblies/groundstation-darmstadt.xml" "-Dhbird.scriptlibrary=scripts" "-Dorekit.data.path=resources" "-Dlog4j.configuration=file:resources/log4j.properties" "-Dhbird.log=groundstation" -jar hbird-0.9.0.jar 
 pause
