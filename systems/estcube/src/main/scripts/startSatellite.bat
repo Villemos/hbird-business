e8b965c6f4b08ee76b818c307ae95d276dc70170
Lauri Kimmel <lauri.kimmel@gmx.com>
2013-07-01 01:51:51 +0300

    Development of version 0.10.0 (0.10.0-SNAPSHOT)

--- systems/estcube/src/main/scripts/startSatellite.bat
+++ systems/estcube/src/main/scripts/startSatellite.bat
@@ -1,2 +1,2 @@
-java "-Dhbird.assembly=assemblies/satellite-estcube1.xml" "-Dhbird.scriptlibrary=scripts" "-Dorekit.data.path=resources" "-Dlog4j.configuration=file:resources/log4j.properties" "-Dhbird.log=satellite" -jar hbird-0.9.0.jar 
+java "-Dhbird.assembly=assemblies/satellite-estcube1.xml" "-Dhbird.scriptlibrary=scripts" "-Dorekit.data.path=resources" "-Dlog4j.configuration=file:resources/log4j.properties" "-Dhbird.log=satellite" -jar hbird-0.10.0-SNAPSHOT.jar 
 pause
