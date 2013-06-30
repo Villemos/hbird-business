f3b888952c42eee264537d1a4c107c585a2f288b
Lauri Kimmel <lauri.kimmel@gmx.com>
2013-07-01 00:54:37 +0300

    Updated version from 0.8.0-SNAPSHOT to 0.9.0-SNAPSHOT

--- systems/estcube/src/main/scripts/startAutoTracking.bat
+++ systems/estcube/src/main/scripts/startAutoTracking.bat
@@ -1,2 +1,2 @@
-java "-Dhbird.assembly=assemblies/tracking-estcube1-darmstadt.xml" "-Dhbird.scriptlibrary=scripts" "-Dorekit.data.path=resources" "-Dlog4j.configuration=file:resources/log4j.properties" "-Dhbird.log=system" -jar hbird-0.8.0-SNAPSHOT.jar 
+java "-Dhbird.assembly=assemblies/tracking-estcube1-darmstadt.xml" "-Dhbird.scriptlibrary=scripts" "-Dorekit.data.path=resources" "-Dlog4j.configuration=file:resources/log4j.properties" "-Dhbird.log=system" -jar hbird-0.9.0-SNAPSHOT.jar 
 pause
--- systems/estcube/src/main/scripts/startWebsocket.bat
+++ systems/estcube/src/main/scripts/startWebsocket.bat
@@ -1,2 +1,2 @@
-java "-Dhbird.assembly=assemblies/websockets.xml" "-Dhbird.scriptlibrary=scripts" "-Dorekit.data.path=resources" "-Dlog4j.configuration=file:resources/log4j.properties" "-Dhbird.log=ui" -jar hbird-0.8.0-SNAPSHOT.jar 
+java "-Dhbird.assembly=assemblies/websockets.xml" "-Dhbird.scriptlibrary=scripts" "-Dorekit.data.path=resources" "-Dlog4j.configuration=file:resources/log4j.properties" "-Dhbird.log=ui" -jar hbird-0.9.0-SNAPSHOT.jar 
 pause
