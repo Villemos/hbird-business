1a671b4598083e1a2fa2d45682950ae555e4f6b8
Lauri Kimmel <lauri.kimmel@gmx.com>
2013-09-09 18:17:25 +0300

    Development of version 0.11.0 (0.11.0-SNAPSHOT)

--- systems/estcube/src/main/deprecated/scripts/startCoreSystem.sh
+++ systems/estcube/src/main/deprecated/scripts/startCoreSystem.sh
@@ -1,1 +1,1 @@
-java -jar -Dhbird.assembly=assemblies/core.xml -Dhbird.scriptlibrary=scripts -Dorekit.data.path=resources -Dlog4j.configuration=file:resources/log4j.properties -Dhbird.log=system hbird-0.10.0.jar 
+java -jar -Dhbird.assembly=assemblies/core.xml -Dhbird.scriptlibrary=scripts -Dorekit.data.path=resources -Dlog4j.configuration=file:resources/log4j.properties -Dhbird.log=system hbird-0.11.0-SNAPSHOT.jar 
--- systems/estcube/src/main/scripts/startES5EC.sh
+++ systems/estcube/src/main/scripts/startES5EC.sh
@@ -1,1 +1,1 @@
-java -jar -Dhbird.assembly=assemblies/gs-ES5EC.xml -Dhbird.scriptlibrary=scripts -Dorekit.data.path=resources -Dlog4j.configuration=file:resources/log4j.properties -Dhbird.log=ES5EC hbird-0.10.0.jar
+java -jar -Dhbird.assembly=assemblies/gs-ES5EC.xml -Dhbird.scriptlibrary=scripts -Dorekit.data.path=resources -Dlog4j.configuration=file:resources/log4j.properties -Dhbird.log=ES5EC hbird-0.11.0-SNAPSHOT.jar
